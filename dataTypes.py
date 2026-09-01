d60173e85b88bc47423b3a7b2e9cd1c1158439ee
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-01 15:50:29 +0530

    Data Types in Python

---
+++ dataTypes.py
@@ -0,0 +1,99 @@
+""" Data Types in Python """
+
+
+#Text Data Type
+
+''' String Data Type '''
+a = "Hello"
+b = "World"
+print(a,b)
+print(type(a))
+
+
+#Numeric Data Types
+
+''' Integer Data Type '''
+c = 10
+d = 20
+print(c,d)
+print(type(c))
+
+''' Float Data Type '''
+e = 10.5
+f = 20.5
+print(e,f)
+print(type(e))
+
+''' Complex Data Type '''
+g = 10+5j
+h = 20+5j
+print(g,h)
+print(type(g))
+
+#Sequence Data types
+
+'''List Data Type '''
+j = [1,2,3,4,5]
+print(j)
+print(type(j))
+
+''' Tuple Data Type '''
+k = (1,2,3,4,5)
+print(k)
+print(type(k))
+
+
+'''Range Data Type '''
+m = range(1,5)
+print(m)
+print(type(m))
+
+#Map Data Type
+''' Dictionary Data Type '''
+l = {"a":1,"b":2,"c":3,"d":4,"e":5}
+print(l)
+print(type(l))
+
+
+#Set Data Type
+''' Set Data Type '''
+i = {1,2,3,4}
+print(i)
+print(type(i))
+
+#Boolean Data Type
+''' Boolean Data Type '''
+n = True
+print(n)
+print(type(n))
+
+#None Data Type
+
+''' None Data Type '''
+o = None
+print(o)
+print(type(o))
+
+
+#Defining Empty Data Types
+
+''' Empty List Data Type '''
+p = []
+print(p)
+print(type(p))
+
+''' Empty Tuple Data Type '''
+q = ()
+print(q)
+print(type(q))
+
+''' Empty Dictionary Data Type '''
+r = {}
+print(r)
+print(type(r))
+
+''' Empty Set Data Type '''
+s = set()
+print(s)
+print(type(s))
+
