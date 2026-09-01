b30cdf16dca17e9571144ae15fc138f7aeeb66e8
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-01 15:51:42 +0530

    Methods to perform of String

---
+++ stringMethods.py
@@ -0,0 +1,68 @@
+s = "Hardhik"
+
+# String Methods
+
+""" Upper() """
+print(s.upper(),"this is upper method")
+
+""" Lower() """
+print(s.lower(),"this is lower method")
+
+""" Title() """
+print(s.title(),"this is title method")
+
+""" Swapcase() """
+print(s.swapcase(),"this is swapcase method")
+
+""" Capitalize() """
+print(s.capitalize(),"this is capitalize method")
+
+""" Center() """
+print(s.center(20,"*"),"this is center method")
+
+""" Ljust() """
+print(s.ljust(20,"*"),"this is ljust method")
+
+""" Rjust() """
+print(s.rjust(20,"*"),"this is rjust method")
+
+"""join() """
+print(",".join(s),"this is join method")
+
+
+"""split() """
+print(s.split(","),"this is split method")
+print(s.split(","),sep="|", end="This is seperator method \n")
+
+"""isalnum() """
+print(s.isalnum(),"returns true if all characters are alphanumeric")
+
+"""isalpha() """
+print(s.isalpha(),"return true if all characters are alphabet")
+
+"""isdigit() """
+print(s.isdigit(),"return true if all characters are digit")
+
+"""islower() """
+print(s.islower(),"return true if all characters are lowercase")
+
+"""isupper() """
+print(s.isupper(),"return true if all characters are uppercase")
+
+"""isspace() """
+print(s.isspace(),"return true if all characters are space")
+
+"""istitle() """
+print(s.istitle(),"return true if all characters are title")
+
+"""isnumeric() """
+print(s.isnumeric(),"return true if all characters are numeric")
+
+"""isidentifier() """
+print(s.isidentifier(),"return true if all characters are identifier")
+
+# there are total of 20 string methods
+
+#match strings 
+
+a = input()
