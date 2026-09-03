04566c5e7b46bc323f2e952eac30b57f5302b75d
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-03 15:23:25 +0530

    Add script to check pass or fail based on marks

---
+++ If-else workout questions/passORfail.py
@@ -0,0 +1,10 @@
+# Take marks input from the user and print:
+# o   "Pass" if marks >= 35
+# o   "Fail" otherwise
+
+marks = int(input())
+
+if marks>=35:
+    print("Pass")
+else:
+    print("fail")
