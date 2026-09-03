ab337e7e4d468651c3c515da244a2cc342c30694
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-03 15:23:30 +0530

    Add script to find the greater of two numbers

---
+++ If-else workout questions/greaterof2.py
@@ -0,0 +1,9 @@
+#    Take two numbers as input and print the greater number.
+
+a = int(input())
+b = int(input())
+
+if a>b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{b} is greater")
