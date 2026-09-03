ba76a8bca4714edfe270fafa8d8afc29ed1f8218
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-03 15:23:20 +0530

    Add script to check voting eligibility based on age

---
+++ If-else workout questions/voter.py
@@ -0,0 +1,9 @@
+#   Ask the user for their age. If the age is 18 or more, print “You are eligible to vote.”
+
+a = int(input())
+
+
+if a>=18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote.")
