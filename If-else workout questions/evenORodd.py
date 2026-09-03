486e5762ce36d82794f7a4b4d53720dd835f332a
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-03 15:23:15 +0530

    Add script to check if a number is even or odd

---
+++ If-else workout questions/evenORodd.py
@@ -0,0 +1,8 @@
+# Take an input number from the user and check whether it is even or odd.
+'''Take input'''
+a = int(input()) 
+
+if a%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
