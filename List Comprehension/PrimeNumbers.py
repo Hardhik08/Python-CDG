ad08b32457c7e84c0867c97d4c654d809226fef9
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-18 17:38:41 +0530

    Add list comprehension script to find prime numbers less than 50

---
+++ List Comprehension/PrimeNumbers.py
@@ -0,0 +1,4 @@
+# Create a list of all prime numbers less than 50.?
+
+x = [i for i in range(2,50) if (i%2 != 0 and i%3 != 0 and i%5 != 0 and i%7 != 0) or (i==2 or i==3 or i ==5 or i ==7)]
+print(x)
