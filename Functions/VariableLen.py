3e8a42d2027d17ac324fd690aa78997a34fc2c66
Hardhik08 <hardhikkurumaddali964@gmail.com>
2026-09-18 17:35:12 +0530

    Add script demonstrating variable-length positional arguments (*args)

---
+++ Functions/VariableLen.py
@@ -0,0 +1,9 @@
+# Variable Length Arguments
+
+# Def:
+# a function can accept a variable number of arguments (can be 0 to n number of arguments)
+
+def List(*a):
+    print(a)
+
+List(1,2,3,4,5,6,7,8,9,10) #Returns the values/args as a tuple.
